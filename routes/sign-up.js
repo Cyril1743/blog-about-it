178fbcec272ab7fd0c8f1d5a2ecd628ae70da41a
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-03-09 18:17:10 -0600

    Added verification for unique users

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -2,6 +2,15 @@ const signup = require("express").Router()
 const path = require("path")
 const fs = require("fs")
 const bcrypt = require('bcrypt');
+const flash = require('express-flash')
+const session = require('express-session')
+
+signup.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}))
+signup.use(flash())
 
 signup.get("/", (req, res) => {
     res.render(path.join(__dirname, "../public/html/newuser.html"));
@@ -11,9 +20,10 @@ signup.post("/", (req, res) => {
     fs.readFile(path.join(__dirname, "../db/users.json"), "utf-8", async (err, data) => {
         if (err) return res.status(404).json("Bad request")
         var parsedData = JSON.parse(data)
-        var userNotAvailable = parsedData.find(user => user.name === req.body.username)
-        if (userNotAvailable) {
-            return req.flash('info', 'User already exists')
+        for (let i = 0; i < parsedData.length; i++){
+            if (parsedData[i].username === req.body.username) {
+                return req.flash('error', 'User already exists'), res.render(path.join(__dirname,"../public/html/newuser.html"))
+            }
         }
         try {
             var id = Math.floor(Math.random() * 10000)
