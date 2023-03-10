0a4660c0e5c6ac064fab85ba04baa4fc82a57fc4
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-03-09 18:02:00 -0600

    Added functions to verify user

--- routes/login.js
+++ routes/login.js
@@ -5,15 +5,13 @@ const fs = require("fs")
 const bcrypt = require('bcrypt');
 const flash = require("express-flash")
 const session = require("express-session");
+const ejs = require('ejs')
 
 login.use(flash())
 login.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {
-        httpOnly: false
-    }
 }))
 login.use(express.urlencoded({ extended: false }))
 
@@ -27,32 +25,34 @@ login.get("/account", (req, res) => {
 })
 //Get route for getting the current users
 login.post("/", (req, res) => {
+    console.log(req.body.username)
     fs.readFile(path.join(__dirname, "../db/users.json"), "utf-8", async (err, data) => {
         if (err) return res.status(500).flash("error", err)
         var parsedData = JSON.parse(data)
         for (let i = 0; i < parsedData.length; i++) {
-            if (parsedData[i].username === req.body[0].username) {
-                console.log(parsedData[i].password, req.body[0].password)
-                if ( await bcrypt.compare(req.body[0].password, parsedData[i].password)) {
-                    return res.status(404), req.flash('error', "Invalid password")
-                } else {
-                req.session.regenerate((err) => {
-                    if (err) next(err)
-                    req.session.user = req.body[0].username
-                    req.session.save((err) => {
+            if (parsedData[i].username === req.body.username) {
+                if ( await bcrypt.compare(req.body.password, parsedData[i].password)) {
+                    var status = "sent"
+                    req.session.regenerate((err) => {
                         if (err) next(err)
-                        res.redirect("/login/account")
+                        req.session.user = req.body.username
+                        req.session.save((err) => {
+                            if (err) next(err)
+                            res.render(path.join(__dirname, "../public/html/account.html"), { name: req.session.user })
+                        })
+                        return status
                     })
-
-                })}
-            } else {
-                res.status(404)
-                req.flash("error", "Invalid password")
-            }
+                } else {
+                    status = "sent"
+                    return req.flash("error", "Invalid password"), res.render(path.join(__dirname, "../public/html/login.html"))
+                }
+            } 
+        }
+        if (status !== "sent"){
+            return req.flash("error", "Invalid username"), res.render(path.join(__dirname, "../public/html/login.html"))
         }
 
     })
 
 })
-
 module.exports = login
